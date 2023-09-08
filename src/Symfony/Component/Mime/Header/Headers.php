9c9e9ac8a2de895098c5ccd70a3a529d1d2dde10
Arnaud POINTET <arnaud.pointet@gmail.com>
2023-09-08 08:52:18 +0000

    symfony/mime does not allow to add some headers as a strings, only as arrays

--- src/Symfony/Component/Mime/Header/Headers.php
+++ src/Symfony/Component/Mime/Header/Headers.php
@@ -147,6 +147,8 @@ final class Headers
             $method = 'addTextHeader';
         } elseif ('addIdentificationHeader' === $method) {
             $method = 'addIdHeader';
+        } elseif ($method === 'addMailboxListHeader' && !\is_array($argument)) {
+            $argument = [$argument];
         }
 
         return $this->$method($name, $argument, $more);
